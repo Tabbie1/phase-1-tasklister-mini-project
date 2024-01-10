4744ed2b4d0873d4403b587f4ba1649d0032e83a
tabitha muriithi <excellentwriter120@gmail.com>
2024-01-10 12:19:39 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const newForm = document.getElementById("create-task-form");
+  const newDescription = document.getElementById("new-task-description");
+ 
+
+  const myTasks = document.getElementById("tasks");
+
+
+  newForm.addEventListener("submit", createNewToDo);
 });
+
+const createNewToDo = event => {
+  event.preventDefault();
+  const newDescription = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = newDescription.value;
+  appendNewTask(newTask);
+  event.target.reset();
+};
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+};
